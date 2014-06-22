9c72609663837409cd5195124e428c5a17633e30
David Hrivna <davidekh@users.noreply.github.com>
2014-06-22 22:59:09 +0200

    Added cacheSolve and description comments for all

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,8 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Here are two functions to allow cache the inversion of matrix and return it if needed
+## First function makeCacheMatrix is utility function keeping the value of original matrix as well as the cached inversion matrix
+## Second function cacheSolve actually returns the inversion matrix from matrix stored in makeCacheMatrix
 
-## makeCacheMAtrix provides the set of methods to set, get and setsolve and getsolve for matrix
+## makeCacheMtrix provides the set of methods to set, get and setsolve and getsolve for matrix
 ## set - sets the matrix content
 ## get - gets the matrix content
 ## setsolve - sets the solve(x) - inversion of the matrix
@@ -22,8 +23,19 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## cacheSolve returns the inversion matrix of matrix stored in makeCacheMatrix
+##   it either returns value chached previously
+##   or it calculates the inversion matrix using function solve(Matrix) and then stores inverted matrix for further use 
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        s <- x$getsolve() ## get the cached value (or NULL if not solved and stored yet)
+        if(!is.null(s)) {
+                message("getting cached data") ## just information that stored version is being used
+                return(s) ## returning of inversion matrix already stored in makeCacheMatrix
+        }
+        data <- x$get()   ## get the contect of matrix stored in makeCacheMatrix to variable data
+        s <- solve(data)  ## actual calculation of inversion from variable data
+        x$setsolve(s)     ## setting the chached value using makeCacheMatrix setsolve 
+        s                 ## returnig of newly created inversion matrix
 }
